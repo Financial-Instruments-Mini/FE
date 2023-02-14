15344b4eda3f1a4315bf2ca0ba44d7a49328ebac
Hyeeeein <khi_9866@naver.com>
2023-02-14 23:49:55 +0900

    feat: Add hover function to button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -7,13 +7,13 @@ interface IButton {
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const Button = ({ text, select = true, onClick }: IButton) => {
+const Button = ({ text, select = false, onClick }: IButton) => {
   return (
     <button
       onClick={onClick}
       className={`px-20 py-6 ${select ? 'bg-main-green' : 'bg-sub-green'}  rounded-3xl text-md font-bold  ${
         select ? 'text-sub-green' : 'text-main-green'
-      } -shadow-basic text-lg`}
+      } -shadow-basic text-lg hover:text-sub-green hover:bg-main-green hover:transition hover:duration-500`}
     >
       {text}
     </button>
