bf55289a0ec4c0e3d0dac8b6097fc709706fcf88
Hyeeeein <khi_9866@naver.com>
2023-02-15 14:35:45 +0900

    fix: Fix main button styling

--- src/components/ui/MainButton.tsx
+++ src/components/ui/MainButton.tsx
@@ -5,7 +5,7 @@ const MainButton = ({ text, select = false, onClick }: IButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className={`px-[4.75rem] py-6 rounded-3xl text-md font-bold -shadow-basic text-lg hover:text-sub-green hover:bg-main-green hover:transition hover:duration-500 
+      className={`w-52 py-6 rounded-3xl text-md font-bold -shadow-basic text-lg hover:text-sub-green hover:bg-main-green hover:transition hover:duration-500 
       ${select ? 'bg-main-green text-sub-green' : 'bg-sub-green text-main-green'}`}
     >
       {text}
