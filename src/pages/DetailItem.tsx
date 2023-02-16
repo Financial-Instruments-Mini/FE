5575e568eebb8e590acf83278db74b23e9a78a99
hwanky <48482406+hwanky@users.noreply.github.com>
2023-02-16 13:55:22 +0900

    Design : 상세 페이지 css 수정

--- src/pages/DetailItem.tsx
+++ src/pages/DetailItem.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import LittleTitle from '../components/LittleTitle';
 import 기업은행 from '../assets/bankicons/금융아이콘_PNG_IBK.png';
+import MainButton from '../components/ui/MainButton';
 
 const DetailItem = () => {
   return (
     <main>
       <LittleTitle title='상세 정보' />
-      <section className='h-fit w-full bg-white border-solid border-2 border-main-green rounded-lg relative'>
-        <header className='flex justify-center mt-10'>
-          <article className='h-20 w-20 rounded-full mr-8 opacity-75'>
+      <section className='h-fit w-full mt-10 pt-10 bg-white -shadow-basic rounded-3xl justify-center'>
+        <header className='flex mt-10 '>
+          <article className='h-20 w-20 rounded-full mr-8 opacity-75 justify-start ml-20'>
             <img src={기업은행} alt='' />
           </article>
-          <article>
+          <article className='justify-end ml-10'>
             <h1 className='text-gray flex flex-col gap-1'>
-              <p className='text-xl font-bold mb-2 leading-tight'>가계우대 정기적금 • 기업은행</p>
+              <p className='text-xl font-bold mb-2 leading-tight'>가계우대 정기적금</p>
+              <p className='font-bold mb-2'>기업은행</p>
               <p className='text-base'>최고 연 4.0%</p>
               <p className='text-base'>최대한도 3000만원</p>
             </h1>
           </article>
         </header>
-        <section className='text-gray mt-10 p-10 flex flex-col'>
-          <p>
+        <section className='text-gray p-20 flex flex-col items-center'>
+          <p className='mb-20'>
             상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세
             정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보
             상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세
@@ -29,7 +31,7 @@ const DetailItem = () => {
             정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보
             상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세 정보 상세
           </p>
-          <button className='mt-20'>버튼 컴포넌트</button>
+          <MainButton text={'신청하기'} />
         </section>
       </section>
     </main>
