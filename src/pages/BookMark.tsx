d809265eeb7c3ef7f1f2968df4206dabaa3d81ad
Kim JiYeong <zmswkfl@naver.com>
2023-02-23 15:26:50 +0900

    feat: pull 받음

--- src/pages/BookMark.tsx
+++ src/pages/BookMark.tsx
@@ -13,18 +13,18 @@ const BookMark = () => {
     <section className='items-center justify-center'>
       <h1 className='flex px-7 pb-5 font-bold text-xl justify-center'>관심 상품</h1>
       <section className='flex flex-wrap text-xs font-base gap-3 text-main-white my-4'>
-        {/* {items &&
+        {items &&
           items.data.map(item => {
             return (
               <section className='relative flex flex-row w-full gap-2'>
-                <ItemCard key={item.id} item={item} />
+                {/* <ItemCard key={item.id} item={item} /> */}
                 <MdDeleteForever
                   className='text-gray text-xl absolute top-2 right-2 cursor-pointer'
                   onClick={handleDeleteClick}
                 />
               </section>
             );
-          })} */}
+          })}
       </section>
     </section>
   );
